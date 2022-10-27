dc31c688e07dfc1560efbc84b48a2ca02049ae9a
DevErickBR <erickyanSP@outlook.com>
2022-10-27 20:14:47 -0300

    add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import { DadosPessoais } from './components/DadosPessoais';
 import { DestinoDeInteresse } from './components/DestinosDeInteresse';
 import styled from './App.module.css';
+import { useState } from 'react'
 
 function App() {
+  const [data, setData] = useState<object[]>([]);
+  const updataDados = (cpf: string, cel: string, email: string, nome: string) => {
+    setData([{ nome: nome, cpf: cpf, email: email, cel: cel }])
+  }
+
   return (
-    <div className={styled.container}>
-      <DadosPessoais />
-      <DestinoDeInteresse />
-    </div >
+    <div className={styled.main}>
+      <div className={styled.container}>
+        <DadosPessoais />
+        <DestinoDeInteresse />
+      </div >
+      <div className={styled.containerButton}>
+        <button>Enviar</button>
+      </div>
+    </div>
   )
 }
 
