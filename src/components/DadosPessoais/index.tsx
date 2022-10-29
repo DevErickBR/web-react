b2989e26df0e77f4a1a538d92e8217e679b54524
DevErickBR <erickyanSP@outlook.com>
2022-10-29 15:37:46 -0300

    finalizado

--- src/components/DadosPessoais/index.tsx
+++ src/components/DadosPessoais/index.tsx
@@ -8,47 +8,60 @@ export const DadosPessoais = () => {
     const [email, setEmail] = useState<string>("")
     const [nome, setNome] = useState<string>("");
 
+
     return (
         <div className={styled.container}>
 
             <h2>Dados Pessoais</h2>
+            <form>
+                <fieldset>
+                    <legend>Nome</legend>
+                    <input
+                        type='text'
+                        value={nome}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNome(e.target.value)}
+                        name="nome"
+                        required
+                    />
 
-            <fieldset>
-                <legend>Nome</legend>
-                <input
-                    type='text'
-                    value={nome}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNome(e.target.value)}
-                />
+                </fieldset>
 
-            </fieldset>
+                <fieldset>
+                    <legend>Email</legend>
+                    <input
+                        type='email'
+                        value={email}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+                        name='email'
+                        required
 
-            <fieldset>
-                <legend>Email</legend>
-                <input
-                    type='email'
-                    value={email}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-                />
-            </fieldset>
+                    />
+                </fieldset>
 
-            <fieldset>
-                <legend>Telefone</legend>
-                <InputMask
-                    mask="(99) 99999-9999"
-                    value={cel}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCel(e.target.value)}
-                />
-            </fieldset>
+                <fieldset>
+                    <legend>Telefone</legend>
+                    <InputMask
+                        mask="(99) 99999-9999"
+                        value={cel}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCel(e.target.value)}
+                        name='telefone'
+                        required
 
-            <fieldset>
-                <legend>CPF</legend>
-                <InputMask
-                    mask="999.999.999-99"
-                    value={cpf}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCpf(e.target.value)}
-                />
-            </fieldset>
+                    />
+                </fieldset>
+
+                <fieldset>
+                    <legend>CPF</legend>
+                    <InputMask
+                        mask="999.999.999-99"
+                        value={cpf}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCpf(e.target.value)}
+                        name='cpf'
+                        required
+                    />
+                </fieldset>
+                <button type='submit'>Enviar</button>
+            </form>
         </div>
     );
 };
